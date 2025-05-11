d1c0e3ebc7ff68d3aec3572c845726f1dd2d3c59
Engineer-Jerry <jerry-mp@hotmail.com>
2025-05-11 14:43:38 -0300

    variáveis_constantes

---
+++ variáveis_constantes.py
@@ -0,0 +1,14 @@
+nome = "Jerry"
+idade = 32
+
+nome, idade = "Carol", 18
+
+print(nome, idade)
+
+limite_saque_diario = 1000
+
+BRAZILIAN_STATES = ["SP", "RJ", "SC", "RS"]
+
+BRAZILIAN_STATES = 32
+
+print(BRAZILIAN_STATES)
